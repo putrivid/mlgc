effb6a471fa0d070e5c3cbeb2c0610dc2bb59798
putrivid <139895011+putrivid@users.noreply.github.com>
2024-05-21 21:07:03 +0700

    Update handler.js

--- src/server/handler.js
+++ src/server/handler.js
@@ -8,36 +8,39 @@ async function postPredictHandler(request, h) {
   const { image } = request.payload;
   const { model } = request.server.app;
  
-  const { label } = await predictClassification(model, image);
+  const { confidenceScore, label, suggestion } = await predictClassification(model, image);
   const id = crypto.randomUUID();
   const createdAt = new Date().toISOString();
- 
+
   const data = {
     "id": id,
     "result": label,
-    "suggestion": label == 'Cancer' ? 'Segera hubungi dokter' : 'None',
+    "suggestion": suggestion,
+    "confidenceScore": confidenceScore,
     "createdAt": createdAt
   }
   
-  await storeData(id, data);
+
 
   const response = h.response({
     status: 'success',
     message: 'Model is predicted successfully',
-    data
+    data: data
   })
   response.code(201);
   return response;
-  
 }
 
 async function getHistoriesHandler(request, h) {
-  const data = await getHistories()
+  const data = await getHistories("\(default\)");
+
   const response = h.response({
-    status: 'success',
-    data: histories
-  })
+      status: "success",
+      data,
+  });
+  response.code(200)
   return response;
 }
+
  
 module.exports = {postPredictHandler, getHistoriesHandler};
