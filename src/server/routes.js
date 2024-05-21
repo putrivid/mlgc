08765a52055b29750035a4b9eca10285a1df04f2
putrivid <139895011+putrivid@users.noreply.github.com>
2024-05-21 09:58:31 +0700

    Add files via upload

---
+++ src/server/routes.js
@@ -0,0 +1,22 @@
+const {postPredictHandler, getHistoriesHandler} = require ('./handler');
+
+const routes = [
+  {
+    path: '/predict',
+    method: 'POST',
+    handler: postPredictHandler,
+    options: {
+      payload: {
+        allow: 'multipart/form-data',
+        multipart: true
+      }
+    }
+  },
+  {
+  path: '/predict/histories',
+  method: 'GET',
+  handler: getHistoriesHandler
+}
+]
+ 
+module.exports = routes;
